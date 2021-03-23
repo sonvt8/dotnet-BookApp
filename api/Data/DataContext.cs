6c3fa42b7a3761386ba0cd080220282dfd2f1988
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2021-03-23 16:45:39 +0700

    rebuild project

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<AppUsers> Users { get; set; }
+    }
+}
---
+++ api/Entities/AppUsers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Entities
+{
+    public class AppUsers
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public bool isAuthor { get; set; }
+    }
+}
